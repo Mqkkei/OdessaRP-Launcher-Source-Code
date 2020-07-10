1ece84ff3a4b193d7002650c9fb89de83b13ee26
Mqkkei <59529588+Mqkkei@users.noreply.github.com>
2020-07-10 20:16:12 +0200

    dugget er også sød
    <3

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdessaRP_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"chrome.exe", "https://discord.com/invite/AUw2aPg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("fivem://connect/95.217.118.181:30120");
+        }
+    }
+}
